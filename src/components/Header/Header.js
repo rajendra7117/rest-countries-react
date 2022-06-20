f37a9b3627f0477cec6958d2ade1a12428200685
rajendra7117 <rajendrabvm0007@gmail.com>
2022-06-20 21:18:47 +0530

    new commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.scss'
 import {MdOutlineLightMode} from 'react-icons/md'
+import {BsMoonFill} from 'react-icons/bs'
 import { useSelector, useDispatch } from 'react-redux'
 import { themeSliceActions } from '../../store/ThemeSlice'
 const Header = () => {
@@ -12,7 +13,7 @@ const toggleTheme = e => {
   return (
     <div className={`header ${darkTheme ? 'dark-theme' : ''}`}>
         <h1>Where in the world?</h1>
-        <button onClick={toggleTheme}><MdOutlineLightMode />Dark Mode</button>
+        <button onClick={toggleTheme}>{!darkTheme ? (<><BsMoonFill />Dark Mode</>) : (<><MdOutlineLightMode />Light Mode</>)}</button>
     </div>
   )
 }
--- src/components/Home/CountryDetails.js
+++ src/components/Home/CountryDetails.js
@@ -2,33 +2,40 @@ import React, { useEffect, useState } from "react";
 import "./CountryDetails.scss";
 import Wrapper from "../Layout/Wrapper";
 import { useSelector } from "react-redux";
+import {IoMdArrowBack} from 'react-icons/io'
+import { useHistory } from "react-router-dom";
 
 const CountryDetails = () => {
   const country = useSelector((state) => state.countryDetails.countryDetails);
-  const darkTheme = useSelector((state) => state.theme.darkTheme)
-  const [borderArray, setBorderArray] = useState([])
+  const darkTheme = useSelector((state) => state.theme.darkTheme);
+  const [borderArray, setBorderArray] = useState([]);
+  const history = useHistory()
   console.log(country);
- 
+
   let borders;
-   country.borders?.forEach((border) => {
-    borders+=`${border},`
-})
-const fetchBorders = async () => {
-    const res = await fetch(`https://restcountries.com/v3.1/alpha?codes=${borders}`)
-    if(!res.ok){
-      return
+  country.borders?.forEach((border) => {
+    borders += `${border},`;
+  });
+  const fetchBorders = async () => {
+    const res = await fetch(
+      `https://restcountries.com/v3.1/alpha?codes=${borders}`
+    );
+    if (!res.ok) {
+      return;
     }
-    const data = await res.json()
-    
-    setBorderArray(data)
-  }
+    const data = await res.json();
+
+    setBorderArray(data);
+  };
   useEffect(() => {
-    fetchBorders()
-  },[])
-  console.log(borderArray)
+    fetchBorders();
+  }, []);
+  console.log(borderArray);
   return (
     <Wrapper>
-      <div className={`country-details ${darkTheme ? 'dark-theme' : ''}`}>
+      
+      <div className={`country-details ${darkTheme ? "dark-theme" : ""}`}>
+      <button className="back-button" onClick={() => history.push('/')}><IoMdArrowBack />back</button>
         <div className="flag-div">
           <img src={country.flags.png} alt="flag" />
         </div>
@@ -37,7 +44,6 @@ const fetchBorders = async () => {
             <div className="sec-1">
               <div className="part-1">
                 <span>
-                
                   <h1>
                     {typeof country.name === "object"
                       ? country.name.common
@@ -53,12 +59,10 @@ const fetchBorders = async () => {
                   {country.population}
                 </span>
                 <span>
-                 
                   <h4>Sub Region:</h4>
                   {country.subregion}
                 </span>
                 <span>
-                
                   <h4>Capital:</h4>
                   {country.capital}
                 </span>
@@ -71,24 +75,32 @@ const fetchBorders = async () => {
                 <span>
                   <h4>Currencies:</h4>
                   {country.currencies?.map((cur, index) => (
-                    <span key={cur.name}>{cur.name}{`${index!==country.currencies.length-1 ? ',' : ''}`}</span>
+                    <span key={cur.name}>
+                      {cur.name}
+                      {`${index !== country.currencies.length - 1 ? "," : ""}`}
+                    </span>
                   ))}
                 </span>
                 <span>
-                <h4>Languages:</h4>
-                {country.languages.map((lan,index) => (
-                  <span key={lan.name}>{lan.name}{index!==country.languages.length-1 ? ',' : ''}</span>
-                ))}
+                  <h4>Languages:</h4>
+                  {country.languages.map((lan, index) => (
+                    <span key={lan.name}>
+                      {lan.name}
+                      {index !== country.languages.length - 1 ? "," : ""}
+                    </span>
+                  ))}
                 </span>
-                
               </div>
             </div>
             <div className="sec-2">
-            <h4>Border Countries: </h4>
-            {borderArray?.map((border) => <span className="border" key={border.name.common}>{border.name.common}</span>)}
+              <h4>Border Countries: </h4>
+              {borderArray?.map((border) => (
+                <span className="border" key={border.name.common}>
+                  {border.name.common}
+                </span>
+              ))}
+            </div>
           </div>
-          </div>
-          
         </div>
       </div>
     </Wrapper>
