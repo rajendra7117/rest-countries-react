e7372ef07a9fb96a8e6ef5e1996cb266e2652e3f
rajendra7117 <rajendrabvm0007@gmail.com>
2022-06-28 23:40:57 +0530

    changes

--- src/store/ThemeSlice.js
+++ src/store/ThemeSlice.js
@@ -2,10 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const themSlice = createSlice({
     name: 'themeSlice',
-    initialState: {darkTheme: false},
+    initialState: {darkTheme: localStorage.getItem('rest-theme') ? JSON.parse(localStorage.getItem('rest-theme')) : false},
     reducers:{
         toggleTheme(state, action){
             state.darkTheme = !state.darkTheme
+            localStorage.setItem('rest-theme', JSON.stringify(state.darkTheme))
         }
     }
 
